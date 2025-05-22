bdaca3fce4b26f0c2a16c1bf3830ffa3f25ee278
Chantha2121 <chanfcsg334@gmail.com>
2025-05-23 00:02:57 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
-
-use Illuminate\Http\Request;
+// Debug environment variables
+error_log('APP_KEY: ' . env('APP_KEY'));
+error_log('APP_CIPHER: ' . env('APP_CIPHER'));
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -8,7 +9,7 @@ $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-$request = Request::capture();
+$request = Illuminate\Http\Request::capture();
 
 $response = $kernel->handle($request);
 $response->send();
--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Default welcome route
+Route::get('/', function () {
+    return view('welcome');
+});
 
