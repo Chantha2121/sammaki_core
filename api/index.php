69dee8d1f964241be6f3685e77a64a99166b05b8
Chantha2121 <chanfcsg334@gmail.com>
2025-05-22 23:40:48 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
-<?php  
-require __DIR__ . "/../public/index.php";
+<?php
+
+use Illuminate\Http\Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Request::capture();
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
