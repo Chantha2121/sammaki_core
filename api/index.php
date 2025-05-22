f394ab5344723f2b89376e5f6a09ddfe5ddd58a2
Chantha2121 <chanfcsg334@gmail.com>
2025-05-23 00:11:11 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,16 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+/** @var Kernel $kernel */
+$kernel = $app->make(Kernel::class);
 
 $request = Request::capture();
 
 $response = $kernel->handle($request);
+
 $response->send();
 
 $kernel->terminate($request, $response);
